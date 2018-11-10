8c6598ead09b1cb686b38a24e0c8f2070235d4fa
Youssef IRAOUI <Youssef.iraoui@gmail.com>
2018-11-10 15:02:41 +0100

    search keword

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import { TweetsComponent } from './tweets/tweets.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+
+const root: Routes = [
+  {path : 'tweets' , component: TweetsComponent},
+  {path : 'header' , component: HeaderComponent}
+  ];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TweetsComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(root),
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
